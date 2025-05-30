fd8139f55f1aeba21da2c3b7bb6509e00f1757fe
Your Name <ayush.0.kumar2224@gmail.com>
2025-05-31 04:16:34 +0530

    Creating the ui

--- src/ui/layout.rs
+++ src/ui/layout.rs
@@ -1,11 +1,66 @@
-use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::{ layout::{Alignment, Constraint, Direction, Layout, Rect}, text::Line, widgets::{self, block::Position, Block, BorderType, Borders, Paragraph}, Frame};
 
-pub fn layout_one (area :Rect)->(Rect , Rect) {
+pub fn base(f : &mut Frame){
+    let area = Rect::new(3, 1, f.size().width - 5, f.size().height-1);
+    let base = Block::new()
+        .borders(Borders::all())
+        .border_type(BorderType::Rounded);
 
-    let method_block = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(8) , Constraint::Percentage(92)])
-        .split(area);
-    (method_block[0] , method_block[1])
+    f.render_widget(base, area);
 }
 
+pub fn method(f : &mut Frame) {
+    let area = Rect::new(5,2,30,4);
+    let widget = Block::new()
+        .borders(Borders::all())
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .title("Method [M]");
+
+    f.render_widget(widget, area);
+}
+
+pub fn url(f : &mut Frame) {
+    let area = Rect::new(36,2,f.size().width - 40,4);
+    let widget = Block::new()
+        .borders(Borders::all())
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .title("Url [U]");
+
+    f.render_widget(widget, area);
+}
+
+pub fn work_space(f : &mut Frame) {
+    let area = Rect::new(5,6,30,f.size().height-7);
+    let widget = Block::new()
+        .borders(Borders::all())
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .title("Workspace [W]");
+
+    f.render_widget(widget, area);
+}
+
+pub fn features(f : &mut Frame) {
+    let area = Rect::new(36,6,f.size().width-40, 15);
+    let widget = Block::new()
+        .borders(Borders::all())
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .title("features");
+
+    f.render_widget(widget, area);
+}
+
+
+pub fn response(f : &mut Frame) {
+    let area = Rect::new(36,21,f.size().width-40, f.size().height-7);
+    let widget = Block::new()
+        .borders(Borders::all())
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded)
+        .title("Response");
+
+    f.render_widget(widget, area);
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,4 +1,1 @@
 pub mod layout;
-pub mod tabs;
-pub mod request_block;
-pub mod respone_block;
