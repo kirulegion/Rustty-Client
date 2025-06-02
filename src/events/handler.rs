f17dbe9d2adba592b352d799e44e5d097e25be49
Your Name <ayush.0.kumar2224@gmail.com>
2025-06-02 13:48:15 +0530

    handling key press for rusty client mode to select different blocks.

---
+++ src/events/handler.rs
@@ -0,0 +1,25 @@
+use ratatui::crossterm::event::{self, KeyCode, KeyEvent};
+
+use crate::state::app_state::{App_state, Focused};
+
+pub fn handle_modes(key: KeyEvent, state: &mut App_state) {
+    match (state.focused, key.code) {
+        //Exit the application when q is pressed in normal.
+        (Focused::Normal, KeyCode::Char('q')) => {
+            state.is_running = false;
+        }
+        //Enter the url block to edit or enter the url.
+        (Focused::Normal, event::KeyCode::Char('U')) => {
+            state.focused = Focused::Url;
+        }
+        //Enter the methods selection block.
+        (Focused::Normal, event::KeyCode::Char('M')) => {
+            state.focused = Focused::Method;
+        }
+        //Entry normal mode when ESC is pressed.
+        (_, event::KeyCode::Esc) => {
+            state.focused = Focused::Normal;
+        }
+        _ => {}
+    }
+}
